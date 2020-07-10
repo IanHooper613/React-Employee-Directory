4b41dcab303d269c13090ac0da9bf4e076d24e86
Ian Hooper <ianhooper8@gmail.com>
2020-07-10 16:15:05 -0400

    adding h1 title on app.js file

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import Container from './components/Container'
+import './components/Container';
+import Container from './components/Container';
 
 function App() {
   return (
     <div className="container">
+      <h1>AppleBook Company Directory</h1>
       <Container />
     </div>
   );
